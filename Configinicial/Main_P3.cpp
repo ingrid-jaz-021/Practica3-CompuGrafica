0fb767a3794b184fde995778653829360f414107
Ingrid Serrano <ingridserrano021@gmail.com>
2026-09-04 23:42:04 -0600

    Avance 2:  Cubo 3

--- Configinicial/Main_P3.cpp
+++ Configinicial/Main_P3.cpp
@@ -224,7 +224,7 @@ int main() {
 		glm::mat4 model = glm::mat4(1);
 		glm::mat4 view = glm::mat4(1);
 
-		// Caja inicial 
+		// Caja inicial  --- Cubo 1 ---
 		view = glm::translate(view, glm::vec3(0.0f, -4.0f, -12.0f)); // Manipular profundidad
 		model = glm::rotate(model, 0.5f, glm::vec3(0.0f, 1.0f, 0.0f)); // use to compare orthographic and perspective projection
 		model = glm::scale(model, glm::vec3(3.0f, 1.5f, 5.0f));
@@ -242,6 +242,7 @@ int main() {
 		glBindVertexArray(VAO);
 		glDrawArrays(GL_TRIANGLES, 0, 36);
 		// Agregar nuevos elementos, inicializando la matriz y agregamos traslación
+		// --- Cubo 2 ---
 		model = glm::mat4(1);
 		model = glm::translate(model, glm::vec3(0.0f, 1.0f, 0.0f));
 		model = glm::rotate(model, 2.5f, glm::vec3(0.0f, 17.0f, 0.0f)); // use to compare orthographic and perspective projection
@@ -250,13 +251,15 @@ int main() {
 		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
 		glDrawArrays(GL_TRIANGLES, 0, 36);
 
-		glBindVertexArray(0);
 
-		// Agregar nuevos elementos, inicializando la matriz y agregamos traslación
-		model = glm::translate(model, glm::vec3(0.0f, 5.0f, -4.0f));
-		model = glm::rotate(model, 0.5f, glm::vec3(2.0f, 0.0f, 1.0f)); // use to compare orthographic and perspective projection
-		model = glm::scale(model, glm::vec3(1.0f, 3.0f, 8.0f));
-		// Antes de mandar info al Shader
+		// --- Cubo 3 ---
+		model = glm::mat4(1);
+		// Caracteristicas
+		model = glm::translate(model, glm::vec3(0.0f, 3.0f, 0.0f));
+		model = glm::rotate(model, 1.2f, glm::vec3(0.0f, 1.0f, 0.0f));
+		model = glm::scale(model, glm::vec3(1.5f, 1.0f, 1.5f));
+
+		// Mandar la info al Shader y dibujar
 		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
 		glDrawArrays(GL_TRIANGLES, 0, 36);
 
